1c25b631d548d53b928353125d49bd4a6e5fbf26
lennard.welslau <lennard.welslau@bruegel.org>
2023-05-07 13:54:35 +0200

    v2

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 0) {
+      $('nav').addClass('scrolled');
+    } else {
+      $('nav').removeClass('scrolled');
+    }
+  });
+});
