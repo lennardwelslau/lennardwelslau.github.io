fb1653acf425e18de685253d5e1ed62d1b1784d2
lennard.welslau <lennard.welslau@bruegel.org>
2023-12-23 05:05:03 +0100

    style

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ $(document).ready(function() {
   });
 });
 
+$(document).ready(function() {
+  // Smooth scrolling for all navigation links
+  $('nav a').on('click', function(e) {
+    if (this.hash !== '') {
+      e.preventDefault();
+
+      const hash = this.hash;
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800, // Adjust the duration as needed
+        function() {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
