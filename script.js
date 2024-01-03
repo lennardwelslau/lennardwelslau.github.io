59ae8566ceccce49af923270fb700af4b9705ce1
lennard.welslau <lennard.welslau@bruegel.org>
2024-01-03 20:37:27 +0100

    cv exception for java script

--- script.js
+++ script.js
@@ -8,20 +8,19 @@ $(document).ready(function() {
     }
   });
 
-  // Smooth scrolling for all navigation links
-  $('nav a').on('click', function(e) {
+  // Smooth scrolling for all navigation links except the CV link
+  $('nav a:not([href="CV/CV_Lennard_Welslau.pdf"])').on('click', function(e) {
     e.preventDefault();
 
     const hash = this.hash;
-    const targetOffset = $(hash).offset().top - 20 ; // Adjust the offset (5 pixels above the target)
+    const targetOffset = $(hash).offset().top - 20;
 
     $('html, body').animate(
       {
         scrollTop: targetOffset
       },
-      800, // Adjust the duration as needed
+      800,
       function() {
-        // Update the hash in the URL after scrolling is complete
         window.location.hash = targetOffset;
       }
     );
